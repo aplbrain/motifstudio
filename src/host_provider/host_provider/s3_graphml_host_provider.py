dbbe82d3133c5ed8cecd593ac4bb72e232020e83
Jordan Matelsky <j6k4m8@gmail.com>
2023-12-19 14:46:49 -0500

    Add s3 graphml cache

--- src/host_provider/host_provider/s3_graphml_host_provider.py
+++ src/host_provider/host_provider/s3_graphml_host_provider.py
@@ -8,12 +8,13 @@ import networkx as nx
 class S3GraphMLHostProvider(GraphMLHostProvider):
     """A Host Provider that can handle S3 URIs."""
 
-    def __init__(self, bucket: str, s3_client=None):
+    def __init__(self, bucket: str, s3_client=None, cache: bool = True):
         """Initialize the provider.
 
         Arguments:
             bucket (str): The S3 bucket to use.
             s3_client (boto3.client): An optional S3 client to use.
+            cache (bool): Whether to cache graphs in a temporary file.
 
         Returns:
             None
@@ -21,6 +22,7 @@ class S3GraphMLHostProvider(GraphMLHostProvider):
         """
         self.bucket = bucket
         self.s3_client = s3_client or boto3.client("s3")
+        self._cache_dir = tempfile.TemporaryDirectory() if cache else None
 
     @property
     def type(self) -> str:
@@ -42,10 +44,18 @@ class S3GraphMLHostProvider(GraphMLHostProvider):
 
         """
         # Save the graph to a temporary file, then read it back in.
-        # This is a workaround for NetworkX, which prevents reading from a
-        # file-like object.
-        # TODO: Cache the graph in a temporary file, so we don't have to
-        # download it every time.
-        with tempfile.NamedTemporaryFile() as f:
-            self.s3_client.download_file(self.bucket, uri[len(f"s3://{self.bucket}/") :], f.name)
-            return super().get_networkx_graph(f.name)
+        # This is a workaround for NetworkX, which cannot read from file-likes.
+        uri_without_scheme = uri.split("://")[-1].replace("/", "___")
+        if self._cache_dir is None:
+            # No caching, just download and return
+            with tempfile.NamedTemporaryFile() as f:
+                self.s3_client.download_file(self.bucket, uri[len(f"s3://{self.bucket}/") :], f.name)
+                return super().get_networkx_graph(f.name)
+        else:
+            # Cache the graph in a temporary file, so we don't re-download:
+            _cache_path = f"{self._cache_dir.name}/{uri_without_scheme}"
+            try:
+                return super().get_networkx_graph(_cache_path)
+            except FileNotFoundError:
+                self.s3_client.download_file(self.bucket, uri[len(f"s3://{self.bucket}/") :], _cache_path)
+                return super().get_networkx_graph(_cache_path)
