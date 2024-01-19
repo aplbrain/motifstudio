53b9b66393cb6198766e0cfdc756fe4c2fb2d76b
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2024-01-18 19:39:02 -0500

    Load data from pickles (#14)

--- src/host_provider/host_provider/__init__.py
+++ src/host_provider/host_provider/__init__.py
@@ -1,6 +1,6 @@
 """This package provides host providers for various types of graph data sources."""
 from .host_provider import HostProvider
-from .filesystem_graphml_host_provider import FilesystemGraphHostProvider
+from .filesystem_graph_host_provider import FilesystemGraphHostProvider
 from .s3_graphml_host_provider import S3GraphMLHostProvider
 from .OpenCypherHostProvider import OpenCypherHostProvider
 
