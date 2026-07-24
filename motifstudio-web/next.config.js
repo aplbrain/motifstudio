d1dad5dccaeca33a017bfc3308748549add01b44
Jordan Matelsky <j6k4m8@gmail.com>
2026-07-24 15:40:37 -0400

    fix: upgrade frontend dependencies

--- motifstudio-web/next.config.js
+++ motifstudio-web/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Don't fail the build when there are ESLint errors
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     // Don't fail the build when there are TypeScript errors
     typescript: {
         ignoreBuildErrors: true,
